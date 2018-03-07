cef7b1ea3e8af767f9364f5c16c3ccc197998875
Annamalai015 <37113799+Annamalai015@users.noreply.github.com>
2018-03-07 14:12:19 +0530

    pattern printing program1

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int i,j,n=5;
+		for(i=0;i<n;i++){
+		    for(j=0;j<=i;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println("");
+		}
+	}
+}
+
+
+
+
