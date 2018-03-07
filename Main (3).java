7aa965857f281ba14e2610645f9b2adf339f8a16
Annamalai015 <37113799+Annamalai015@users.noreply.github.com>
2018-03-07 14:49:14 +0530

    pattern printing - number pyramid

---
+++ Main (3).java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int i, j, n = 5, k = 2 * n - 2, num = 1;
+    for (i = 0; i < n; i++)
+      {
+
+	for (j = 0; j < k; j++)
+	  {
+	    System.out.print (" ");
+	  }
+	k = k - 1;
+	for (j = 0; j <= i; j++)
+	  {
+	    System.out.print (num + " ");
+	    num++;
+	  }
+	System.out.println ("");
+      }
+  }
+}
